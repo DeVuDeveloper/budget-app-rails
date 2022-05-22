7ccaad37c2bab9108d5b460e19ae0a992f195f66
VuDej <drvu47@gmail.com>
2022-05-22 17:06:27 +0200

    added welcome page and style for it

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,5 @@ Rails.application.routes.draw do
   resources :payments
   resources :categories
   resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :welcome, only: [:index]
 end
