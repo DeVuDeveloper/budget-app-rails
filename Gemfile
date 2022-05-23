812213bd792f5ef0388ac2e7ab0e564ac2f4c47e
VuDej <drvu47@gmail.com>
2022-05-23 07:46:06 +0200

    installed devise and fixed errors with rails 7

--- Gemfile
+++ Gemfile
@@ -75,3 +75,5 @@ group :development, :test do
 end
 
 gem 'rubocop', '>= 1.0', '< 2.0'
+
+gem 'devise', '~> 4.8'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    # Redirests signing out users back to sign-in
+    get "users", to: "devise/sessions#new"
+  end
+  
+  devise_for :users
   root 'categories#index'
 
   get 'categories/most_recent_list'
