eb65188c42abff4f1ab4943f4eda9dd41b69e320
VuDej <drvu47@gmail.com>
2022-05-23 11:02:52 +0200

    added rspec tests for models

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Category validation tests' do
+    before(:each) do
+      @user = User.new(name: 'John', email: 'john@mail.com', password: 'john')
+      @category = @user.categories.new(user_id: @user.id, name: 'Power',
+                                       icon: 'https://cdn.vectorstock.com/i/1000x1000/95/52/electric-lighting-icon-power-symbol-vector-27939552.webp')
+    end
+
+    it 'if category has name' do
+      @category.name = nil
+      expect(@category).to_not be_valid
+    end
+
+    it ' name is string' do
+      @category.name = 'Power'
+      expect(@category).to be_valid
+    end
+  end
 end
--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Payment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validates' do
+    before(:each) do
+      @user = User.new(name: 'John', email: 'john@mail.com', password: 'john')
+      @category = @user.categories.new(user_id: @user.id, name: 'Power',
+                                       icon: 'https://cdn.vectorstock.com/i/1000x1000/95/52/electric-lighting-icon-power-symbol-vector-27939552.webp')
+
+      @payment = Payment.new(user_id: @user.id, name: 'No 12345', amount: 24, category_id: 8)
+    end
+
+    it 'if there is no name' do
+      @payment.name = nil
+      expect(@payment).to_not be_valid
+    end
+
+    it 'if amount is empty' do
+      @payment.amount = nil
+      expect(@payment).to_not be_valid
+    end
+
+    it 'if category is empty' do
+      @payment.category_id = nil
+      expect(@payment).to_not be_valid
+    end
+  end
 end
